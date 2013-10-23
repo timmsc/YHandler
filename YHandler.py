6aa031c7833969b195417e121a41b7d3764fb1c9
Sean Timm <sean@event.com>
2013-10-23 00:34:21 -0400

    added (untested) access token refresh

--- YHandler.py
+++ YHandler.py
@@ -1,9 +1,12 @@
 import requests
 from rauth import OAuth1Service
 from requests import request
+from rauth.utils import parse_utf8_qsl
 import webbrowser
 import csv
 import os.path
+import pickle as pk
+import rauth.service as serv
 
 GET_TOKEN_URL = 'https://api.login.yahoo.com/oauth/v2/get_token'
 AUTHORIZATION_URL = 'https://api.login.yahoo.com/oauth/v2/request_auth'
@@ -17,7 +20,8 @@ class YHandler(object):
 	def __init__(self, authf):
 		self.authf = authf
 		self.authd = self.get_authvals_csv(self.authf)
-		self.session = None
+		self.session = self.deserialize_session()
+		#self.session = None
 		# Get a real consumer key & secret
 		self.yahoo = OAuth1Service(
 			name='yahoo',
@@ -26,6 +30,7 @@ class YHandler(object):
 			request_token_url=REQUEST_TOKEN_URL,
 			access_token_url=GET_TOKEN_URL,
 			authorize_url=AUTHORIZATION_URL,
+			#session_obj=self.session,
 			base_url='https://api.login.yahoo.com/oauth/v2/')
 
 	def get_authvals_csv(self, authf):
@@ -45,16 +50,44 @@ class YHandler(object):
 		f_iter.writerow(authd)
 		f.close
 
+	def serialize_session(self, session):
+		return pk.dump(session, open('session.dat', 'w+'))
+
+	def deserialize_session(self):
+        	try:
+	           with open('session.dat'): pass
+        	except IOError:
+	           print 'Oh dear. Couldn\'t open session file'
+		   return None
+		return pk.load(open('session.dat'))
+
 	def new_session(self):
 		request_token, request_token_secret = self.yahoo.get_request_token(data = { 'oauth_callback': CALLBACK_URL })
 		auth_url = self.yahoo.get_authorize_url(request_token)
 		print 'Visit this URL in your browser: ' + auth_url
 		pin = raw_input('Enter PIN from browser: ')
-		session = self.yahoo.get_auth_session(request_token, request_token_secret, method='POST', data={'oauth_verifier': pin})
+               # same as OAuth1Service.get_auth_session, but also returns oauth_session_handle which is need by Yahoo's OAuth to refresh the access token
+                r = self.yahoo.get_raw_access_token(request_token,
+                                                    request_token_secret,
+                                                    method='POST',
+                                                    data={'oauth_verifier': pin})
+
+                access_token, access_token_secret, session_handle = \
+                        serv.process_token_request(r, parse_utf8_qsl, 'oauth_token', 'oauth_token_secret', 'oauth_session_handle')
+
+
 		f = open(sessionf, 'wb')
-		f.write(session.access_token + '\n')
-		f.write(session.access_token_secret + '\n')
-		return session
+		f.write(access_token + '\n')
+		f.write(access_token_secret + '\n')
+		f.write(session_handle + '\n')
+		session = self.yahoo.get_session((access_token, access_token_secret))
+		if self.yahoo.request_token_response:
+            		session.request_token_response = self.yahoo.request_token_response
+        	if self.yahoo.access_token_response:
+           		session.access_token_response = self.yahoo.access_token_response
+
+		self.serialize_session(session)
+                return session
 
 	def reuse_session(self):
 		f = open(sessionf, 'rb')
@@ -62,20 +95,52 @@ class YHandler(object):
                 access_token_secret = f.readline().rstrip('\n')
 		print 'access_token: ' + access_token
 		print 'access_token_secret: ' + access_token_secret
-		session = self.yahoo.get_session((access_token, access_token_secret))
+		session = self.yahoo.get_session((access_token, access_token_secret), self.yahoo.signature_obj)
 		return session
+
+	def refresh_session(self):
+                f = open(sessionf, 'rb')
+                access_token = f.readline().rstrip('\n')
+                access_token_secret = f.readline().rstrip('\n')
+		session_handle = f.readline().rstrip('\n')
+                print 'access_token: ' + access_token
+                print 'access_token_secret: ' + access_token_secret
+                print 'session_handle: ' + session_handle
+                r = self.yahoo.get_raw_access_token(request_token,
+                                                    request_token_secret,
+						    data={'session_handle': session_handle})		    
+	        access_token, access_token_secret, session_handle = \
+                        serv.process_token_request(r, parse_utf8_qsl, 'oauth_token', 'oauth_token_secret', 'oauth_session_handle')
+
+
+                f = open(sessionf, 'wb')
+                f.write(access_token + '\n')
+                f.write(access_token_secret + '\n')
+                f.write(session_handle + '\n')
+                session = yahoo.get_session((access_token, access_token_secret))
+                if self.yahoo.request_token_response:
+                        session.request_token_response = self.yahoo.request_token_response
+                if self.yahoo.access_token_response:
+                        session.access_token_response = self.yahoo.access_token_response
+
+                self.serialize_session(session)
+                return session
+
+	
 
 	def api_req(self, querystring, req_meth='GET', data=None, headers=None):
 		base_url = 'http://fantasysports.yahooapis.com/fantasy/v2/'
 		url = base_url + querystring
-		if (self.session is None):
-			if (os.path.exists(sessionf)):
-				self.session = self.reuse_session()
-			else:
-				self.session = self.new_session()
+		if (self.session is None and not os.path.exists(sessionf)):
+			self.session = self.new_session()
+		else:
+			self.session = self.reuse_session()
 		print "calling url: ", url
 		r=self.session.get(url)
-	
+		if (r.status_code != 200):
+			print "Got status code: " + r.status_code
+			self.refresh_session()
+			r=self.session.get(url)	
 		return r
 
 	
